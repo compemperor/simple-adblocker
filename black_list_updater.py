9cae276da73edb0611d792c6a28d5f675c69034b
compemperor <saiddem99@hotmail.com>
2017-08-10 00:07:35 +0200

    Add files via upload

---
+++ black_list_updater.py
@@ -0,0 +1,158 @@
+#This file walks out to the great internet and updates the blocked domains list
+
+# See `https://github.com/StevenBlack/hosts` for details
+#compemperor
+
+import urllib2
+import re
+
+##StevenBlack's list
+steve_ = "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+
+##MalwareDomains
+malw_ = "https://mirror1.malwaredomains.com/files/justdomains"
+
+##Cameleon
+came_ = "http://sysctl.org/cameleon/hosts"
+
+##Zeustracker
+zeus_ = "https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist"
+
+##Disconnect.me Tracking
+simtrck_ = "https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt"
+
+##Disconnect.me Ads
+simad_ ="https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt"
+
+##Hosts-file.net
+ad_serv_ = "https://hosts-file.net/ad_servers.txt"
+
+
+def Update_steve_():
+    data = ""
+    try:
+        print "Updating Steves list: %s" %steve_
+        data=urllib2.urlopen(steve_).read()
+        data = data.replace("127.0.0.1", "")
+        data = data.replace('0.0.0.0',"")
+        data = data.replace('localhost.localdomain',"")
+        data = data.replace('255.255.255.255',"")
+        data = data.replace('broadcasthost',"")
+        data = data.replace('localhost',"")
+        data = data.replace('local',"")
+        data = data.replace('::1',"")
+        data = re.sub(r"fe80%lo0", "", data)
+
+
+    except Exception,e:
+        print "Can not get StevenBlac's list %s" %e
+    finally:
+        return data
+
+def Update_malw_():
+    data = ""
+    try:
+        print "Updating Malware domains: %s" %malw_
+        data=urllib2.urlopen(malw_).read()
+
+    except Exception,e:
+        print "Can not get MalwareDomains list %s" %e
+    finally:
+        return data
+
+def Update_came_():
+    data = ""
+    try:
+        print "Updating Cameleon: %s" %came_
+        data=urllib2.urlopen(came_).read()
+        data = data.replace("127.0.0.1","")
+
+    except Exception,e:
+        print "Can not get Cameleon list %s" %e
+    finally:
+        return data
+
+def Update_zeus_():
+    data = ""
+    try:
+        print "Updating Zeustracker: %s" %zeus_
+        data=urllib2.urlopen(zeus_).read()
+
+    except Exception,e:
+        print "Can not get Zeustracker list %s" %e
+    finally:
+        return data
+
+def Update_simtrck_():
+    data = ""
+    try:
+        print "Updating disconnect.me Tracking: %s" %simtrck_
+        data=urllib2.urlopen(simtrck_).read()
+
+    except Exception,e:
+        print "Can not get Disconnect.meTracks list %s" %e
+    finally:
+        return data
+
+def Update_simad_():
+    data = ""
+    try:
+        print "Updating disconnect.me Ads: %s" %simad_
+        data=urllib2.urlopen(simad_).read()
+
+    except Exception,e:
+        print "Can not get Disconnect.meAds list %s" %e
+    finally:
+        return data
+
+def Update_ad_serv_():
+    data = ""
+    try:
+        print "Updating Host-file.net: %s" %ad_serv_
+        data=urllib2.urlopen(ad_serv_).read()
+        data = data.replace("127.0.0.1", "")
+
+    except Exception,e:
+        print "Can not get Hostfile.net list %s" %e
+    finally:
+        return data
+
+def Update_user_list():
+    data = ""
+    try:
+        print "Updating user list: ./user_list.txt"
+        user_list= open("user_list.txt",'a+')
+        data = user_list.read()
+        user_list.close()
+    except Exception, e:
+        print "can not update the user list %s" %e
+    finally:
+        return  data
+
+
+
+def Join_and_save():
+    data = ""
+    try:
+        host_file= open("hosts.txt","w+")
+        data = Update_steve_() + Update_malw_() + Update_came_() + Update_zeus_() \
+        + Update_simtrck_() + Update_simad_() + Update_ad_serv_() + Update_user_list()
+        print "Formating the hosts file...."
+        data = re.sub(r'#.*', "", data)
+
+
+        reformatted = ""
+        for li in data.split('\n'):
+            if li.strip():
+                reformatted += li + '\n'
+
+
+        host_file.write(reformatted)
+        host_file.close()
+    except Exception, e:
+        print "Something went wrong when writing host.txt %s" %e
+
+if __name__ == '__main__':
+    print "Updating host list this might take a while...."
+    Join_and_save()
+   
---
+++ blocker.sh
@@ -0,0 +1,142 @@
+#!/bin/bash 
+
+cat <<BeginOfArt
+
+  ________  __     ___      ___    _______   ___       _______           __       ________   _______   ___        ______    ______   __   ___  _______   _______   
+ /"       )|" \   |"  \    /"  |  |   __ "\ |"  |     /"     "|         /""\     |"      "\ |   _  "\ |"  |      /    " \  /" _  "\ |/"| /  ")/"     "| /"      \  
+(:   \___/ ||  |   \   \  //   |  (. |__) :)||  |    (: ______)        /    \    (.  ___  :)(. |_)  :)||  |     // ____  \(: ( \___)(: |/   /(: ______)|:        | 
+ \___  \   |:  |   /\\  \/.    |  |:  ____/ |:  |     \/    |         /' /\  \   |: \   ) |||:     \/ |:  |    /  /    ) :)\/ \     |    __/  \/    |  |_____/   ) 
+  __/  \\  |.  |  |: \.        |  (|  /      \  |___  // ___)_       //  __'  \  (| (___\ ||(|  _  \\  \  |___(: (____/ // //  \ _  (// _  \  // ___)_  //      /  
+ /" \   :) /\  |\ |.  \    /:  | /|__/ \    ( \_|:  \(:      "|     /   /  \\  \ |:       :)|: |_)  :)( \_|:  \\        / (:   _) \ |: | \  \(:      "||:  __   \  
+(_______/ (__\_|_)|___|\__/|___|(_______)    \_______)\_______)    (___/    \___)(________/ (_______/  \_______)\"_____/   \_______)(__|  \__)\_______)|__|  \___) 
+compemperor                                                                                                                                                    1.0
+
+BeginOfArt
+ 
+cat <<BeginOfOpt
+
+Welcome to simple adblocker, this is nothing more than
+a script that uses dnsmasq and python to block ads/malware domains. 
+The script requiers that dnsmasq and python 2.7.x to be installed, it may/will change your existing dnsmasq
+confgurations.(apt-get install dnsmasq should do it)
+
+start - start/update dnsmasq and populate it with the latest blocked domain names, run it once in awhile to update the domain list 
+
+stop - stop dnsmasq and remove the firewall rules 
+
+abort - you may guess this one
+
+BeginOfOpt
+
+#------------------Global declartions---------------------
+
+#Stop/start dnsmasq 
+stopcmd='/etc/init.d/dnsmasq stop'
+startcmd='/etc/init.d/dnsmasq start'
+statuscmd='/etc/init.d/dnsmasq status'
+
+#Update the hosts 
+update_hosts='python ./black_list_updater.py'
+
+out_interface=$(route | grep '^default' | grep -o '[^ ]*$')
+
+#redirect traffic to this ip  
+catcherip='127.0.0.1'
+
+#dnsmasq default configuration file
+configfile=/etc/dnsmasq.conf 
+
+#just a temp file to save domains to
+tmpconffile="/tmp/.dnsmasq.conf.$$"
+
+#will hold the blocked domains, this file is alterd by the python code
+hosts_file="./hosts.txt"
+
+
+
+# Add/delete firewall/iptabel rules to drop the packets forwarded to the catcher 
+firewall_rule_check="iptables -C OUTPUT -o $(echo out_interface) -p udp --dport 53  -d 127.0.0.1 -j DROP"
+firewall_rule_add="iptables -A OUTPUT -o $(echo out_interface) -p udp --dport 53  -d 127.0.0.1 -j DROP"
+firewall_rule_delete="iptables -D OUTPUT -o $(echo out_interface) -p udp --dport 53  -d 127.0.0.1 -j DROP"
+
+#---------------------------Logic---------------------------
+
+#Check if the hosts file exist
+function check_host_file
+{ 
+	if [ ! -s $1 ] 
+	then		
+		echo "hosts file '$1' either doesn't exist or is empty; quitting"
+		exit 
+	fi 
+}
+
+#Populate dnsmasq 
+function dnsmasq_config
+{
+	cat $1 | grep -v "address=" > $2
+
+	while read line; do
+		ADDRESS="/${line}/${3}"
+		echo "address=\"${ADDRESS}\"" >> $2
+	done < $4
+
+	mv $2 $1 
+
+}
+
+
+echo "Please select an option:"
+select opt in "start" "stop" "status" "abort"
+do
+	if [ $opt == "abort" ]
+	then
+		echo "goodbye..."
+		break
+	
+	elif [ $opt == "status" ]
+	then
+		$statuscmd
+		break
+
+	elif [ $opt == "start" ] 
+   	then 
+		#stop dnsmasq if it is runing
+		$stopcmd
+		echo "Adding firewall rules..."
+		#check if the rule exits
+		$firewall_rule_check 2> /dev/null
+		if [ $? -eq 1 ]
+		then
+			#add firewall rules
+			$firewall_rule_add
+		fi
+		#update the domain list
+		$update_hosts
+		#check if the hostfile is there
+		check_host_file $hosts_file
+		#populate dnsmasq 
+		dnsmasq_config $configfile $tmpconffile $catcherip $hosts_file
+		#start dnsmasq
+		$startcmd
+		echo "Done..."
+		break
+
+
+	elif [ $opt == "stop" ] 
+   	then 
+		#stop dnsmasq if it is runing
+		$stopcmd
+		echo "Deleting firewall rules..."
+		#check if the rule exits
+		$firewall_rule_check 2> /dev/null
+		if [ $? -eq 0 ]
+		then
+			#delete the firewall rule
+			$firewall_rule_delete
+		fi
+		
+		echo "Done..."
+		break
+	fi
+done 
